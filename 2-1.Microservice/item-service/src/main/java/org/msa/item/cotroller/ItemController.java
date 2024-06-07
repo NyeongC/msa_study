731e6bae42129392cbf765a2a812134c667ee189
NyeongC <cjfsud23@naver.com>
2024-06-07 20:21:19 +0900

    사용자 정의 유효성 검사 어노테이션 추가

--- 2-1.Microservice/item-service/src/main/java/org/msa/item/cotroller/ItemController.java
+++ 2-1.Microservice/item-service/src/main/java/org/msa/item/cotroller/ItemController.java
@@ -7,7 +7,9 @@ import org.msa.item.dto.ItemDTO;
 import org.msa.item.dto.ResponseDTO;
 import org.msa.item.dto.constant.ItemType;
 import org.msa.item.service.ItemService;
+import org.msa.item.valid.ItemTypeValid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,30 +18,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "v1/item")
 @Slf4j
 @RequiredArgsConstructor
+@Validated
 public class ItemController {
 
 	private final ItemService itemService;
 	
 	@RequestMapping(value="/add/{itemType}", method = RequestMethod.POST)
-	public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO, @PathVariable String itemType) {
+	public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO, @ItemTypeValid @PathVariable String itemType) {
 		ResponseDTO.ResponseDTOBuilder responseBuilder = ResponseDTO.builder();
-
-		log.debug("path.variable itemType = {}", itemType);
-		boolean hasItemType = false;
-		ItemType [] itemTypeList = ItemType.values();
-		for(ItemType i : itemTypeList) {
-			if (i.hasItemCd(itemType)) {
-				hasItemType = true;
-				break; // ItemType을 찾았으면 루프를 종료합니다.
-			}
-		}
-
-		if(!hasItemType) {
-			responseBuilder.code("500").message("invalid itemType .[" + itemType + "]");
-			return ResponseEntity.ok(responseBuilder.build());
-		}else {
-			itemDTO.setItemType(itemType);
-		}
 
 		itemDTO.setItemType(itemType);
 		itemService.insertItem(itemDTO);
