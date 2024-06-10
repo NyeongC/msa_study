98cf56eb291d31e3258a21918b36fa30af45d933
NyeongC <cjfsud23@naver.com>
2024-06-10 22:40:56 +0900

    swagger-ui/index.html

--- 2-1.Microservice/item-service/src/main/java/org/msa/item/cotroller/ItemController.java
+++ 2-1.Microservice/item-service/src/main/java/org/msa/item/cotroller/ItemController.java
@@ -1,5 +1,10 @@
 package org.msa.item.cotroller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.msa.item.domain.Item;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 
+@OpenAPIDefinition(info = @Info(title = "물품 처리요청 API", description = "물품 처리요청 API", version = "v1"))
 @RestController
 @RequestMapping(value = "v1/item")
 @Slf4j
@@ -23,7 +29,12 @@ import lombok.extern.slf4j.Slf4j;
 public class ItemController {
 
 	private final ItemService itemService;
-	
+
+	@Operation(summary = "물품등록 요청", description = "물품 등록을 진행할 수 있다.", tags = { "addItem" })
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "SUCCESS"),
+			@ApiResponse(responseCode = "501", description = "API EXCEPTION")
+	})
 	@RequestMapping(value="/add/{itemType}", method = RequestMethod.POST)
 	public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO, @ItemTypeValid @PathVariable String itemType) throws Exception{
 		ResponseDTO.ResponseDTOBuilder responseBuilder = ResponseDTO.builder();
@@ -31,12 +42,6 @@ public class ItemController {
 		itemDTO.setItemType(itemType);
 		itemService.insertItem(itemDTO);
 		log.debug("requset add item id = {}", itemDTO.getId());
-
-		try {
-			Integer.parseInt("test");
-		}catch (Exception e) {
-			throw new ApiException("test에러");
-		}
 		
 		responseBuilder.code("200").message("success");
 		return ResponseEntity.ok(responseBuilder.build());
